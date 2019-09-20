466e76d6ae0aec3b2668b897052dcc66dd50ce48
coldice1915 <coldice.zs44@gmail.com>
2019-09-20 13:46:11 -0700

    Add configuration

--- movies.py
+++ movies.py
@@ -8,3 +8,16 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+actionGenre = MovieGenre(name="Action")
+
+session.add(actionGenre)
+session.commit()
+session.query(MovieGenre).all()
+
+avengers = Movie(name="The Avengers",
+                 summary="Coming together from separate movies, good guys with superpowers band together to fight the bad guy.", rating="8.0", movie_genre=actionGenre)
+
+session.add(avengers)
+session.commit()
+session.query(Movie).all()
