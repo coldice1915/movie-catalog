c487df6ff139b6c59b9a6eabf03d6674941cd5e5
coldice1915 <coldice.zs44@gmail.com>
2019-09-20 10:19:02 -0700

    Add configuration

---
+++ database_setup.py
@@ -0,0 +1,15 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+
+
+
+engine = create_engine('sqlite:///')
+Base.metadata.create_all(engine)
