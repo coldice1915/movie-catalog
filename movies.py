7a219ba4adc365e91e2c8aac2eaa1670d6de980a
coldice1915 <coldice.zs44@gmail.com>
2019-09-20 18:37:50 -0700

    Change 'moviegenre' to 'genre'

--- movies.py
+++ movies.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, User, MovieGenre, Movie
+from database_setup import Base, User, Genre, Movie
 
 
 engine = create_engine('sqlite:///moviecatalog.db')
@@ -9,16 +9,19 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-actionGenre = MovieGenre(name="Action")
+actionGenre = Genre(name="Action")
 
 session.add(actionGenre)
 session.commit()
-session.query(MovieGenre).all()
+session.query(Genre).all()
 
 avengers = Movie(name="The Avengers",
-                 summary="Coming together from separate movies, good guys with superpowers band together to fight the bad guy.", rating="8.0", moviegenre=actionGenre)
+                 summary="Coming together from separate movies, good guys with superpowers band together to fight the bad guy.", rating="8.0", genre=actionGenre)
 
 session.add(avengers)
 session.commit()
 session.query(Movie).all()
 
+movies = session.query(Movie).all()
+for movie in movies:
+    print movie.name
