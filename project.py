992f4f6c3c563c8a844ab2c379b08dea4c666cf5
coldice1915 <coldice.zs44@gmail.com>
2019-09-20 19:11:54 -0700

    Add new/edit/delete routes

--- project.py
+++ project.py
@@ -1,11 +1,47 @@
+from database_setup import Base, User, Genre, Movie
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from flask import Flask
 app = Flask(__name__)
 
+
+engine = create_engine('sqlite:///moviecatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/genre/<int:genre_id>/')
+def genreMovie(genre_id):
+    genre = session.query(Genre)
+    movies = session.query(Movie)
+    output = ''
+    for m in movies:
+        output += m.name
+        output += '<br>'
+        output += m.rating
+        output += '<br>'
+        output += m.summary
+    return output
+
+
+@app.route('/genre/<int:genre_id>/new/')
+def newMovie(genre_id):
+    return "new movie page"
+
+
+@app.route('/genre/<int:genre_id>/<int:movie_id>/edit/')
+def editMovie(genre_id, movie_id):
+    return "edit movie page"
+
+
+@app.route('/genre/<int:genre_id>/<int:movie_id>/delete/')
+def deleteMovie(genre_id, movie_id):
+    return "delete movie page"
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
