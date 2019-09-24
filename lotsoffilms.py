b778d19f46c2a499442aa983fd1edc83ebffb4d5
coldice1915 <coldice.zs44@gmail.com>
2019-09-24 13:55:59 -0700

    Create fake/example data

---
+++ lotsoffilms.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, User, Genre, Film
+
+
+engine = create_engine('sqlite:///moviecatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Action Films
+actionGenre = Genre(name="Action")
+
+session.add(actionGenre)
+session.commit()
+
+film1 = Film(name="Avengers", summary="Heroes coming together from different movies, the good guys with superpowers band together to fight the bad guy.", rating="8.0", whatType="Movie", link=[Trailer]https://www.youtube.com/watch?v=eOrNdBpGMv8/Trailer, genre=actionGenre)
+
+session.add(film1)
+session.commit()
+
+
+# Comedy Films
+comedyGenre = Genre(name="Comedy")
+
+session.add(comedyGenre)
+session.commit()
+
+film2 = Film(name="The Big Lebowski", summary="Jeff 'The Dude' Lebowski, mistaken for a millionaire of the same name, seeks restitution for his ruined rug and enlists his bowling buddies to help get it.", rating="8.5", whatType="Movie", link="", genre=comedyGenre)
+
+session.add(film2)
+session.commit()
