3497c8d53bbd36da146bdfd738e850751fdb6a62
coldice1915 <coldice.zs44@gmail.com>
2019-09-24 14:00:19 -0700

    Change class from Movie to Film

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,7 +11,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    name = Column(String(80), nullable=False)
+    name = Column(String(250), nullable=False)
     id = Column(Integer, primary_key=True)
 
 
@@ -22,18 +23,40 @@ class Genre(Base):
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
-class Movie(Base):
-    __tablename__ = 'movie'
+
+class Film(Base):
+    __tablename__ = 'film'
 
     name = Column(String(250), nullable=False)
     id = Column(Integer, primary_key=True)
     summary = Column(String(250))
-    rating = Column(String(80))
+    rating = Column(String(4))
+    whatType = Column(String(250))
+    link = Column(String(250))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
     genre_id = Column(Integer, ForeignKey('genre.id'))
     genre = relationship(Genre)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'summary': self.summary,
+            'rating': self.rating,
+            'whatType': self.whatType,
+            'link': self.link
+        }
 
 
 engine = create_engine('sqlite:///moviecatalog.db')
